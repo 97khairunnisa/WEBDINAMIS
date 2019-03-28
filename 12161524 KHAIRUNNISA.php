4410d0abf9bbe716976d901787b30854aff5a95a
97khairunnisa <49027247+97khairunnisa@users.noreply.github.com>
2019-03-28 18:53:30 +0700

    Add files via upload
    TUGAS WEB DINAMIS

---
+++ 12161524 KHAIRUNNISA.php
@@ -0,0 +1,16 @@
+<?php
+
+	$makanan = [
+		['bakso' => ['asli' => 'yogyakarta', 'harga' => '10.000', 'rasa' => 'daging sapi']], 
+		['soto' => ['asli' => 'lamongan', 'harga' => '10.000', 'rasa'=> 'ayam']], 
+		['nasikuning' => ['asli' => 'tasik', 'harga' => '50.000', 'rasa' => 'enak']], 
+		['pempek' => ['asli' => 'pelembang', 'harga' => '20.000','rasa' => 'ikan tenggiri']], 
+		['tempoyak' => ['asli' => 'bengkulu','harga' => '100.000', 'rasa'=> 'durian']], 
+		
+	];
+
+	for ($i=0;$i<1;$i++)
+	{
+		echo '<pre>'; print_r($minuman);
+	};
+?>
